17c6d7943bce422277df220819ddd7e5cfb4ee7a
Marsovska <magi.bucka@gmail.com>
2025-02-22 00:16:32 -0500

    something changed

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i < 10; i++) {
+        for (int i = 1; i < 11; i++) {
 
             System.out.println("Hello and welcome!");
 
